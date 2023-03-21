06c1bcf140114af8504407e80f2a6b482fd028d9
M10Z <giacomoandreotti@yahoo.com>
2023-03-21 12:06:05 +0100

    Aggiunto la possibilità di arrestare il menù

--- CSVtoJSON_converter_Pyhton/menù.py
+++ CSVtoJSON_converter_Pyhton/menù.py
@@ -40,10 +40,18 @@ def aprifile():
     global cfg
     cfg = filedialog.askopenfilename(filetypes=[(".ini Files", ".ini .txt")])
 
+def annulla():
+    risposta=messagebox.askyesno(title="Chiudere il menù di selezione?",message="Chiudere il programma?")
+    if (risposta==True):
+        finestra.destroy()
+    else:
+        pass
+
 
 finestra = Tk()
 finestra.title("menù di selezione")
 finestra.geometry("200x170")
+finestra.configure(bg='green')
 selezione1 = Button(text="Seleziona file di configurazione...",command=aprifile)
 
 selezione2 = Button(text="Seleziona directory di uscita...",command=diruscita)
@@ -53,14 +61,15 @@ selezione3 = Button(text="Seleziona file di input...",command=dirinput)
 selezione4 = Button(text="Seleziona directory file di log...",command=dirlog)
 
 
-
+arresta = Button(text="Annulla",command=annulla)
 chiudi = Button(text="Applica",command=stop)
 
 selezione1.pack()
 selezione4.pack()
 selezione3.pack()
 selezione2.pack()
-chiudi.pack()
+arresta.place(x=140,y=140)
+chiudi.place(x=10,y=140)
 
 finestra.mainloop()
 
@@ -70,7 +79,7 @@ try:
         for line in linee:
             f.write(line)
 except FileNotFoundError as err:
-    messagebox.showerror ("ERRORE", "Nessun file di configurazione selezionato")
+    messagebox.showerror ("ATTENZIONE", "Nessun file di configurazione selezionato, il file rimarrà invariato")
 
 
 
