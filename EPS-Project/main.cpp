7ddcb710f5af855a51abf16cfc3a85c8c7f1206c
Fede3398 <casarotto.98@gmail.com>
2023-03-27 15:23:37 +0200

    hghh

--- EPS-Project/main.cpp
+++ EPS-Project/main.cpp
@@ -9,3 +9,5 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+// mioli is used to connect to the server and listen for requests from the server side of the connection process and responses
