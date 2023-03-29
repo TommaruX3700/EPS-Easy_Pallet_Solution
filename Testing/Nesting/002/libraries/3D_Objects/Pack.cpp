636d3193e340be67f0dfe15eac7d21781131b521
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-03-29 12:12:59 +0200

    Test Nesting 2 - setup

--- Testing/Nesting/002/libraries/3D_Objects/Pack.cpp
+++ Testing/Nesting/002/libraries/3D_Objects/Pack.cpp
@@ -4,30 +4,6 @@ Declare here all the 3D objects used inside the project.
 
 #include ".\Geometry.h"
 
-class Pack
-{
-private:
-    struct centerCoords
-    {
-        int x, y, z; //coordinates (divide cms by 10)
-
-    };
-    struct dimsFromCentrer
-    {
-        int x, y, z; //populate dividing dimensions by 2
-
-    };
-    
-    float weight; //[kg]
-    int density; //[Kg/m3]
-
-public:
-    void setCenterCoords(int x, int y, int z);
-    void setDims(int x, int y, int z);
-    int* getDims(); //define what type to pass back for 3 dimensions
-
-};
-
 void Pack::setCenterCoords(int x, int y, int z){ //sets center coords
     centerCoords.x = x;
     centerCoords.y = y;
---
+++ Testing/Nesting/002/libraries/Geometry/Grid.cpp
@@ -0,0 +1,10 @@
+/*
+Declare here all the Geometry entities in used by the project.
+*/
+
+#include ".\3D_Objects.h"
+
+Grid :: popolateGrid(int x, int y, int z){
+    Griglia[][][];
+
+}
