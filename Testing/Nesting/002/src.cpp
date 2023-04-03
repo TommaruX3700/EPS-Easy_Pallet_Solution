0dfdf0692aafc764ab02ecc7ac065d2f75ff275f
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-04-03 09:19:29 +0200

    modified paths

--- Testing/Nesting/002/src.cpp
+++ Testing/Nesting/002/src.cpp
@@ -1,14 +1,20 @@
 #pragma region "General Libraries"
     #include <iostream>
+
 #pragma endregion
 
 #pragma region "Custom Made Libraries"
-    #include ".\libraries\3D_Objects.h"
-    #include ".\libraries\Geometry.h"
+    #include ".\libraries\3D_Objects\Pack.h"
+    #include ".\libraries\3D_Objects\PalletGroup.h"
+    #include ".\libraries\3D_Objects\SinglePallet.h"
+    
+    #include ".\libraries\Geometry\Grid.h"
+    #include ".\libraries\Geometry\Point.h"
+
 #pragma endregion
 
 using namespace std;
 
 int main(){
-
+    co
 }
