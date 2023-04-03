d4ccc4f41373096824786e4479323f2f07ae4034
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-04-03 12:40:27 +0200

    Things

--- Testing/Nesting/002/libraries/3D_Objects/Pack.cpp
+++ Testing/Nesting/002/libraries/3D_Objects/Pack.cpp
@@ -4,26 +4,53 @@ Declare here all the 3D objects used inside the project.
 
 #include ".\Pack.h"
 #include ".\Objects_includes.h"
+#include "..\Geometry\Geom_includes.h"
 
-void Pack::set_CenterCoords(int x, int y, int z){ //sets center coords
-    centerCoords.x = x;
-    centerCoords.y = y;
-    centerCoords.z = z;
-
-}
-
-void Pack::set_Dims(int x, int y, int z){ //set ddimensions divided by 2 (in order to refer at them by center)
-    dimsFromCentrer.x = x/2;
-    dimsFromCentrer.y = y/2;
-    dimsFromCentrer.z = z/2;
+void Pack::set_Dims(float x, float y, float z){ //set ddimensions divided by 2 (in order to refer at them by center)
+    dims.x = x/2;
+    dims.y = y/2;
+    dims.z = z/2;
 
 }
 
-dimensions Pack::getDims(){
+void Pack::set_CenterCoords(float x, float y, float z){ //sets center coords    
+    center.cX = x;
+    center.cY = y;
+    center.cZ = z;
 
-    return temp;
 }
 
-void Pack::changeOrientation(int a){
+void Pack::set_weight(float kg){
+    weight = kg;
+    density = weight/((get_Dims()->x)*(get_Dims()->y)*(get_Dims()->z))
+
+}
+
+dimensions* Pack::get_Dims(){
+    dimensions out;
+    out.x = out.x*2;
+    out.y = out.y*2;
+    out.z = out.z*2;
+    return out*;
+}
+
+void Pack::set_Orientation(int a){
+    switch (a)
+    {
+        case 0:
+            dims.x
+            break;
+        
+        case 1:
+
+            break;
+        
+        case 2:
+        
+            break;
+
+        default:
+            break;
+    }
 
 }
--- Testing/Nesting/002/libraries/3D_Objects/Pack.h
+++ Testing/Nesting/002/libraries/3D_Objects/Pack.h
@@ -1,28 +1,33 @@
 class Pack
 {
     private:
-        typedef struct dimensions
+        typedef struct dimensions //defines type "dimensions"
         {
-            float x, y, z; //populate dividing dimensions by 2
-        };
-        typedef struct centerCoords
+            float x, y, z; //divided by 2
+        }; 
+        typedef struct centerCoords //defines type "centerCoords"
         {
             float cX, cY, cZ;
         };
 
         float weight; //[kg]
-        int density; //[Kg/m3]
+        float density; //[Kg/m3]
+
+        dimensions dims;
+        centerCoords center;
 
     public:
-        Pacco(float X, float Y, float Z, float CX, float CY, float CZ){
-            x=X; y=Y; z=Z; cX=CX; cY=CY; cZ=CZ;
-        }
-        
-        void set_CenterCoords(float x, float y, float z);
         void set_Dims(float x, float y, float z);
+        void set_CenterCoords(float x, float y, float z);
+        void set_weight(float kg);
+        void set_Orientation(int a); //change orientation by setting number here (0, 1 or 2), but multiplies x "1" or "-1"
 
-        dimensions get_Dims(); //define what type to pass back for 3 dimensions
-        void change_Orientation(int a); //change orientation by setting number here (0, 1 or 2), but multiplies x "1" or "-1"
+        dimensions* get_Dims(); //outs a pointer to an out stucture (with correct dimensions)
 
-    
+        Pacco(float X, float Y, float Z, float CX, float CY, float CZ, float WGHT){ //constructor
+            set_Dims(X, Y, Z);
+            set_CenterCoords(CX, CY, CZ);
+            set_weight(WGHT);
+        }
+
 };
