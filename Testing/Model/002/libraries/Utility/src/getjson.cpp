f81c82244b918d6b1ffaa910395bca4d5bd25720
Fede3398 <casarotto.98@gmail.com>
2023-04-06 09:51:23 +0200

    aaa

---
+++ Testing/Model/002/libraries/Utility/src/getjson.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include "../../3D_Objects/headers/Pack.h"
+#include "../../external/rapidjson/document.h"
+#include "../../external/rapidjson/rapidjson.h"
+using namespace std;
+
+
+
+void getJson(int i, Pack pacco, rapidjson::Document& document, vector<Pack>& pacchi, vector<Pack>& pacchiNP){
+    float x, y, z, peso;
+    int n_collo;
+    bool ruotabile;
+
+    if(document[i]["BASE_MAGGIORE"].GetType()==6){
+        x = document[i]["BASE_MAGGIORE"].GetDouble();
+    } else {
+        x = stod(document[i]["BASE_MAGGIORE"].GetString());
+    }
+
+    if(document[i]["BASE_MINORE"].GetType()==6){
+        y = document[i]["BASE_MINORE"].GetDouble();
+    } else {
+        y = stod(document[i]["BASE_MINORE"].GetString());
+    }
+
+    if(document[i]["ALTEZZA"].GetType()==6){
+        z = document[i]["ALTEZZA"].GetDouble();
+    } else {
+        z = stod(document[i]["ALTEZZA"].GetString());
+    }
+
+    if(document[i]["PESO_LORDO"].GetType()==6){
+        peso = document[i]["PESO_LORDO"].GetDouble();
+    } else {
+        peso = set_weight(stod(document[i]["PESO_LORDO"].GetString()));
+    }
+
+    ncollo = (document[i]["NUMERO_COLLO"].GetInt());
+
+    if(document[i]["FLAG_RUOTABILE"].GetString()==""){
+        ruotabile=true;
+    } else {
+        ruotabile=false;
+    }
+
+    Pack pacco=Pack(x, y, z, peso, n_collo, ruotabile);
+
+    if(document[i]["FLAG_PALLETTIZZABILE"].GetString()==""){
+        pacchi.push_back(pacco);
+    } else {
+        pacchiNP.push_back(pacco);
+    }
+}
