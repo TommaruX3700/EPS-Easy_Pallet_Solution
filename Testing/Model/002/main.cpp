e9f9516071aa39c39b46db261d82155e35e4c113
Tommaso Maruzzo <tommasomaruzzo@gmail.com>
2023-04-08 13:44:53 +0200

    refined things

--- Testing/Model/002/main.cpp
+++ Testing/Model/002/main.cpp
@@ -1,28 +1,22 @@
 #pragma region "General Libraries"
-
     #include <iostream>
     #include <vector>
-
 #pragma endregion
-
 
 #pragma region "External Libraries"
-
     #include ".\libraries\external\rapidjson\document.h"
     #include ".\libraries\external\rapidjson\rapidjson.h"
-
 #pragma endregion
 
-
 #pragma region "Custom Made Libraries"
-
     #include ".\libraries\3D_Objects\headers\Pack.h"
     //#include ".\libraries\3D_Objects\headers\PalletGroup.h"
     //#include ".\libraries\3D_Objects\headers\SinglePallet.h"
     //#include ".\libraries\Geometry\headers\2DPlane.h"
+#pragma endregion
 
+#pragma region "Utilities"
     #include ".\libraries\Utility\src\inputmanager.cpp"
-
 #pragma endregion
 
 using namespace std;
